7fd31f3643e519eda4d156960f99707e695e4f8d
ENTELLiJENSE <ENTELLiJENSE@192.168.2.11>
2015-01-31 14:57:29 -0700

    Base class for contribution

---
+++ Tree of life/src/byui/cit260/TreeOfLife/model/Contribution.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.TreeOfLife.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author ENTELLiJENSE
+ */
+public class Contribution implements
+Serializable{
+   
+    private double amount;
+    private String category;
+
+    public Contribution() {
+    }
+
+    
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Contribution{" + "amount=" + amount + ", category=" + category + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.amount) ^ (Double.doubleToLongBits(this.amount) >>> 32));
+        hash = 29 * hash + Objects.hashCode(this.category);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Contribution other = (Contribution) obj;
+        if (Double.doubleToLongBits(this.amount) != Double.doubleToLongBits(other.amount)) {
+            return false;
+        }
+        if (!Objects.equals(this.category, other.category)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+}
+
+
