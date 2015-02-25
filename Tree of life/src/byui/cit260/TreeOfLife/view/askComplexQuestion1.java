c4d112d0f5cfd50391be5610e842e42a2ee05ba8
Caleb <Caleb@Caleb-PC>
2015-02-24 17:35:55 -0700

    This is the individual Assignment for Lesson 6/Lesson 7. I have added a menu to ask the first complex question involving the hyptenuse and right triangle, and have also finished the GameMenuView which is the "Pause Menu" inside of the game so to speak. I also started TempleMenuView but realized after I had designed the MENU that I did not need to do that. Brett if you want to take control of the TempleMenuView you're more than welcome to!

---
+++ Tree of life/src/byui/cit260/TreeOfLife/view/askComplexQuestion1.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.TreeOfLife.view;
+
+import byui.cit260.TreeOfLife.control.TempleMenuControl;
+import byui.cit260.TreeOfLife.model.TempleMenuQuestions;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Caleb
+ */
+public class askComplexQuestion1 {
+    public void run() {
+        TempleMenuQuestions complexQuestion1 = new TempleMenuQuestions();
+        complexQuestion1.setAnswer(7.07);
+        double correctAnswer = complexQuestion1.getAnswer();
+        complexQuestion1.setQuestion("\n\nNephi is building his ship to travel"
+                + "\nacross the ocean, he needs to figure out how tall and wide"
+                + "\nthe ship is. If a diagonal crossmember of the ship is " + correctAnswer
+                + "\nfeet long. How tall and wide is the ship?"
+                + "\n(HINT: The crossmember is the hypetenuse of a right triangle"
+                + "\nand the width and height of the ship are the sides of the"
+                + "\ntriangle!)");
+        String question = complexQuestion1.getQuestion();
+                
+        System.out.println(question);
+        TempleMenuControl calculateComplexQuestion1 = new TempleMenuControl();
+        int height;
+        int width;    
+        int loop = -1;
+        int timesAsked = 0;
+        
+        while (loop != 0 && timesAsked < 3) {
+            height = getHeight();
+            width = getWidth();
+            loop = calculateComplexQuestion1.calculateComplexQuestion(correctAnswer, height, width);
+            if (loop != 0 && timesAsked != 2) {
+                System.out.println("Sorry! You got the answer incorrect! Try again!\n");
+            }
+            timesAsked++;
+        }
+        
+        if (timesAsked == 1){
+            System.out.println("Congratz! You got the question right the first"
+                    + "\ntime! Your reward is (item inserted here)!");
+        }
+        else if (timesAsked < 4) {
+            System.out.println("You got the correct answer, but it took you"
+                    + "\ntoo many guesses! Sorry :(");
+        }
+        else {
+            System.out.println("Sorry! You got the answer incorrect too many,"
+                    + "\ntimes!");
+        }
+    }
+
+    private int getHeight() {
+        System.out.println("How tall is the ship?");
+        Scanner input = new Scanner(System.in);
+        int height = input.nextInt();
+        return height;
+    }
+
+    private int getWidth() {
+        System.out.println("How wide is the ship?");
+        Scanner input = new Scanner(System.in);
+        int width = input.nextInt();
+        return width;
+    }
+}
